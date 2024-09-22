56ff9cc3a1f1824b9f17970f1aed07feee8f1747
PSYCHEER <42911029+PSYCHEER@users.noreply.github.com>
2024-09-22 19:01:36 +0200

    Initial changes

---
+++ src/main/java/eu/psycheer/psyCommands/PsyCommands.java
@@ -0,0 +1,17 @@
+package eu.psycheer.psyCommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PsyCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
