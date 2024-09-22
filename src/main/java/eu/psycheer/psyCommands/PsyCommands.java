a7aea85a58edfb06d1dd48c7df007ac8d2027406
PSYCHEER <42911029+PSYCHEER@users.noreply.github.com>
2024-09-22 21:24:36 +0200

    Vault + LP
    Working LP hookage trough Vault to use Chat API for prefixes

--- src/main/java/eu/psycheer/psyCommands/PsyCommands.java
+++ src/main/java/eu/psycheer/psyCommands/PsyCommands.java
@@ -1,24 +1,36 @@
 package eu.psycheer.psyCommands;
 
+import eu.psycheer.psyCommands.events.PlayerMessage;
 import net.milkbowl.vault.chat.Chat;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class PsyCommands extends JavaPlugin {
 
-    public static Chat chat = null;
+    public Chat chat = null;
+    public File dataFile;
     @Override
     public void onEnable() {
         if(getServer().getPluginManager().getPlugin("LuckPerms") == null){
-            this.getLogger().severe(String.format("[%s] - Disabled because no Vault was found!", getDescription().getName()));
+            this.getLogger().severe(String.format("[%s] - Disabled because no LuckPerms was found!", getDescription().getName()));
             this.getServer().getPluginManager().disablePlugin(this);
         }
+
+        if(this.getDataFolder().exists()){
+            return;
+        }else{
+            //this.dataFile = new File(this.getDataFolder(), saveDefaultConfig());
+        }
+
         setupChat();
         if(!setupChat() || chat == null){
             this.getLogger().severe(String.format("[%s] - Disabled because no Vault was found!", getDescription().getName()));
             this.getServer().getPluginManager().disablePlugin(this);
         }else{
             this.getLogger().warning("Hooked into Chat " + chat +" API!");
+            new PlayerMessage(this, chat);
             this.getLogger().warning("PsyCommands successfully enabled!");
         }
     }
--- src/main/java/eu/psycheer/psyCommands/events/PlayerMessage.java
+++ src/main/java/eu/psycheer/psyCommands/events/PlayerMessage.java
@@ -1,4 +1,34 @@
 package eu.psycheer.psyCommands.events;
 
-public class PlayerMessage {
+import eu.psycheer.psyCommands.PsyCommands;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.milkbowl.vault.chat.Chat;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerMessage implements Listener{
+    public Chat messageConstructor;
+    public Audience viewers;
+    public final PsyCommands plugin;
+
+    public PlayerMessage(PsyCommands plugin, Chat chat){
+        this.plugin = plugin;
+        messageConstructor = chat;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        plugin.getLogger().warning("PlayerMessage event is listening...");
+    }
+
+    @EventHandler
+    private void PlayerSentMessage(AsyncChatEvent e){
+        Player player = e.getPlayer();
+        String playerName = e.getPlayer().getName();
+        String wip = messageConstructor.getPlayerPrefix(player) + playerName;
+        player.setDisplayName(wip);
+
+
+    }
 }
